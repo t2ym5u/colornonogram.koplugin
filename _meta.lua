850cda41802d9e9527740d55ff30c650c669e6fd
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-28 16:29:20 +0200

    chore: bump version to 1.1.7

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ return {
     name        = "colornonogram",
     fullname    = _("Color Nonogram"),
     description = _("Colored picross / nonogram"),
-    version     = "1.1.6",
+    version     = "1.1.7",
 }
