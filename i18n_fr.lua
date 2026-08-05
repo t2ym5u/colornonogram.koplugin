b2430f768371579564a3e736784fe7dfb76d6fa1
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-08-05 14:59:48 +0200

    feat: add ES and DE translations

--- i18n_fr.lua
+++ i18n_fr.lua
@@ -1,5 +1,5 @@
 return {
-    ["%1\xC3\x97%2 \xC2\xB7 %3 \xC2\xB7 Filled: %4/%5"] = { fr = "%1×%2 · %3 · Remplies : %4/%5" },
-    ["Color Nonogram"]                        = { fr = "Nonogramme couleur" },
-    ["Colored picross / nonogram"]            = { fr = "Picross / nonogramme en couleurs" },
+    ["%1\xC3\x97%2 \xC2\xB7 %3 \xC2\xB7 Filled: %4/%5"] = { fr = "%1×%2 · %3 · Remplies : %4/%5", es = "%1×%2 · %3 · Rellenas: %4/%5", de = "%1×%2 · %3 · Gefüllt: %4/%5" },
+    ["Color Nonogram"]                        = { fr = "Nonogramme couleur", es = "Nonograma de color", de = "Farb-Nonogramm" },
+    ["Colored picross / nonogram"]            = { fr = "Picross / nonogramme en couleurs", es = "Picross / nonograma en color", de = "Farbiges Picross / Nonogramm" },
 }
